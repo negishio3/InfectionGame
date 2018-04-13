875b98cbffabf83f358ee9bce703ae639e1fd4d4
mase <nkc.yuya.mase.2016@gmail.com>
2018-04-13 12:53:23 +0900

    スペース押すと色変更

---
+++ Assets/mase/ChangeCollar_mase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCollar_mase : MonoBehaviour
+{
+    public Material[] _matrrial;
+    private int i;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        i = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            i++;
+            if (i == 3)
+            {
+                i = 0;
+            }
+            this.GetComponent<Renderer>().material = _matrrial[i];
+        }
+	}
+}

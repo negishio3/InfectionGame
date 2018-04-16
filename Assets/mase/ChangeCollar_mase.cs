c3cd4c64a089872ef12eb1475d9610035c41c017
mase <nkc.yuya.mase.2016@gmail.com>
2018-04-16 11:15:03 +0900

    まだまだ製作中

--- Assets/mase/ChangeCollar_mase.cs
+++ Assets/mase/ChangeCollar_mase.cs
@@ -7,25 +7,31 @@ public class ChangeCollar_mase : MonoBehaviour
     public Material[] _matrrial;
     private int i;
 
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Start()
     {
+
 
         i = 0;
 
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            i++;
-            if (i == 3)
-            {
-                i = 0;
-            }
-            this.GetComponent<Renderer>().material = _matrrial[i];
-        }
-	}
+        //if (Input.GetKeyUp(KeyCode.Space))
+        //{
+        //    i++;
+        //    if (i == 1)
+        //    {
+        //        i = 0;
+        //    }
+        //    this.GetComponent<Renderer>().material = _matrrial[i];
+        //    this.tag = ("Player");
+        //}
+    }
+
+
+
 }

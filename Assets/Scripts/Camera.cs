f85c438c729c7ac081c782134ba3f5ef420853bc
sanoki <nkc.hirokazu.sanoki@gmail.com>
2018-04-23 14:00:57 +0900

    中断

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Camera : MonoBehaviour {
-    public GameObject Target;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.LookAt(Target.transform.position);
-		
-	}
+
+    Vector3 camera_Rotate;
+    [SerializeField]
+    float rotation_Speed;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            camera_Rotate.y = -1;
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            camera_Rotate.y = 1;
+        }
+        else if (Input.GetKeyUp(KeyCode.O) || Input.GetKeyUp(KeyCode.P))
+        {
+            camera_Rotate.y = 0;
+        }
+        transform.Rotate(camera_Rotate * Time.deltaTime * rotation_Speed);
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,7 +14,8 @@ public class Player : MonoBehaviour {
         rd = GetComponent<Rigidbody>();
     }
 	
-	void Update () {
+	void Update ()
+ {
         if(MoveSpeed >= 0)MoveSpeed += Input.GetAxis("Mouse ScrollWheel");
         if(MoveSpeed < 0) MoveSpeed = 0;
         if (Input.GetButtonDown("Jump"))
@@ -24,5 +25,10 @@ public class Player : MonoBehaviour {
         playerPos.x = Input.GetAxisRaw("Horizontal");
         playerPos.z = Input.GetAxisRaw("Vertical");
         transform.position += playerPos * MoveSpeed * Time.deltaTime;
+    }
+
+    public void ColorChange()
+    {
+
     }
 }

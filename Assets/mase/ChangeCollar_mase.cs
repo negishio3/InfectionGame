24429e9b42392222e84f7608af61acf71d82d6ec
mase <nkc.yuya.mase.2016@gmail.com>
2018-04-23 14:02:17 +0900

    スクリプトからプレハブを取得するところがうまくいかない

--- Assets/mase/ChangeCollar_mase.cs
+++ Assets/mase/ChangeCollar_mase.cs
@@ -4,45 +4,15 @@ using UnityEngine;
 
 public class ChangeCollar_mase : MonoBehaviour
 {
-    public Material[] _matrrial;
-    private int i;
-    Renderer cube;
-
-
-    // Use this for initialization
-    void Start()
-    {
-
-
-        //i = 0;
-        //cube = GameObject.Find("mob").GetComponent<Renderer>();
-
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //if (Input.GetKeyUp(KeyCode.Space))
-        //{
-        //    i++;
-        //    if (i == 1)
-        //    {
-        //        i = 0;
-        //    }
-        //    this.GetComponent<Renderer>().material = _matrrial[i];
-        //    this.tag = ("Player");
-        //}
-    }
-
+    
     public void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
-        {
-            cube = GameObject.Find("mob").GetComponent<Renderer>();
-            cube.material.color = Color.blue;
-            this.tag = ("Player");
+        if (other.tag != transform.tag)
+        { 
+            Renderer enemyMaterial = other.GetComponent<Renderer>();
+            Renderer PlayerMaterial = GetComponent<Renderer>();
+            enemyMaterial.material = PlayerMaterial.material;
+            other.tag = (transform.tag);
         }
     }
-
 }

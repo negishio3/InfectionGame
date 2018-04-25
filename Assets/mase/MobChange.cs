542b144760f1097d20fe9b73b7dc2bb91722173a
mase <nkc.yuya.mase.2016@gmail.com>
2018-04-25 13:47:46 +0900

    煙作成中

--- Assets/mase/MobChange.cs
+++ Assets/mase/MobChange.cs
@@ -4,32 +4,31 @@ using UnityEngine;
 
 public class MobChange : MonoBehaviour
 { 
-   public GameObject[] _MobZombie;
+    GameObject[] _MobZombie;
     void Start()
     {
-        //_MobZombie[0] = (GameObject)Resources.Load("mob(R)");
+        _MobZombie[0] = Resources.Load<GameObject>("mob(R)");
     }
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag != transform.tag)
         {
-            Debug.Log(transform.tag);
             Destroy(other.gameObject);
             switch (transform.tag)
             {
                 case "Red":
                     Instantiate(_MobZombie[0], other.transform.position, Quaternion.identity);
                     break;
-                case "Blue":
-                    Instantiate(_MobZombie[1], other.transform.position, Quaternion.identity);
-                    break;
-                case "Green":
-                    Instantiate(_MobZombie[2], other.transform.position, Quaternion.identity);
-                    break;
-                case "Yellow":
-                    Instantiate(_MobZombie[3], other.transform.position, Quaternion.identity);
-                    break;
+                //case "Blue":
+                //    Instantiate(_MobZombie[1], other.transform.position, Quaternion.identity);
+                //    break;
+                //case "Green":
+                //    Instantiate(_MobZombie[2], other.transform.position, Quaternion.identity);
+                //    break;
+                //case "Yellow":
+                //    Instantiate(_MobZombie[3], other.transform.position, Quaternion.identity);
+                //    break;
             }
         }
     }

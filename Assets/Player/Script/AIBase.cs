4177f2057efdb6c7a36695ccb8a280686f797514
takagi <nkc.ryousuke.takagi@gmail.com>
2018-05-07 15:01:22 +0900

    エントリー画面、AI

--- Assets/Player/Script/AIBase.cs
+++ Assets/Player/Script/AIBase.cs
@@ -11,30 +11,49 @@ public abstract class AIBase : PlayerNumber {
     protected float runSpeed = 3.7f;
     [SerializeField, Header("obj探索範囲")]
     protected float searchDistance;
+    [SerializeField, Header("視野角")]
+    protected float viewAngle=0.85f;
 
 
     protected NavMeshAgent navMeshAgent;
-    protected GameObject[] targetMobs;
-    protected GameObject target;
-    protected int num;
-    protected string targetTag;
-    protected string scriptName;
+    protected GameObject[] targetMobs;          //対象候補
+    protected GameObject target;                //現在の対象
+    protected Vector3 Mypos;                    //自分の現在地
+    protected Vector3 nextPos;                  //次の移動地点
+    protected int num;                          //対象を選ぶ番号
+    protected int loseSightTime = 5;            //見失うまでの時間
+    protected float targetDistance;             //ターゲットと次の移動場所の距離
+    protected float nextPosDistance;            //次の場所までの距離
+    protected float stopTrackingTime;           //0になったら見失う
+    protected bool TrackingFlg;                 //見失ったかどうか
+    protected string targetTag;                 //対象のタグ
 
 
 
     protected virtual void Start ()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-	}
+        stopTrackingTime = 0;
+    }
 
 	protected virtual void Update ()
     {
+        if (stopTrackingTime <= 0)
+        {
+            target = null;
+            TrackingFlg = false;
+        }
+        else
+        {
+            stopTrackingTime -= Time.deltaTime;
+        }
         InSight();
+
 	}
 
     protected abstract void OnTriggerEnter(Collider col);
 
-    protected void InSight()
+    protected void InSight()//視界に入っているか確認
     {
         SearchObj(targetTag, out targetMobs);
         if (targetMobs.Any())
@@ -45,6 +64,8 @@ public abstract class AIBase : PlayerNumber {
                 if (RayCheck())
                 {
                     target = targetMobs[num];
+                    stopTrackingTime = loseSightTime;
+                    TrackingFlg = true;
                     return;
                 }
                 else
@@ -55,13 +76,13 @@ public abstract class AIBase : PlayerNumber {
         }
     }
 
-    bool RayCheck()//視界に入っているか
+    bool RayCheck()//レイが通るならtrue
     {
-        if (targetMobs[num] != null)
+        if (targetMobs.Any())//対象が存在するか
         {
             Vector3 dir = targetMobs[num].transform.position - transform.position;
 
-            if (ViewingAngle(dir))
+            if (ViewingAngle(dir))//視界に入っているか
             {
                 Ray ray = new Ray(transform.position, dir);
                 RaycastHit hit;
@@ -73,8 +94,6 @@ public abstract class AIBase : PlayerNumber {
                         Debug.Log("Insight");
                         return true;
                     }
-
-
                     return false;
                 }
                 return false;
@@ -88,12 +107,12 @@ public abstract class AIBase : PlayerNumber {
         return false;
     }
 
-    bool ViewingAngle(Vector3 dir)//正面にいるか確認
+    bool ViewingAngle(Vector3 dir)//正面にいるならtrue
     {
         dir.Normalize();
         float dot = Vector3.Dot(dir, transform.forward);
         float rad = Mathf.Acos(dot);
-        if (rad < 0.85)
+        if (rad < viewAngle)//視野角に入っているか
         {
             return true;
         }
@@ -103,18 +122,37 @@ public abstract class AIBase : PlayerNumber {
         }
     }
 
-    void SearchObj(string tag, out GameObject[] objs)//範囲内のタグのobjを近い順に取得する
+    void SearchObj(string tag, out GameObject[] objs)//条件を満たしているobjを近い順に取得する
     {
         if (GameObject.FindGameObjectWithTag(tag))
         {
             objs = GameObject.FindGameObjectsWithTag(tag).
-            Where(e => Vector3.Distance(transform.position, e.transform.position) < searchDistance).
-            Where(e => e.GetComponent<PlayerNumber>().PlayerNum != playerNum).
-            OrderBy(e => Vector3.Distance(transform.position, e.transform.position)).ToArray();
+            Where(e => Vector3.Distance(transform.position, e.transform.position) < searchDistance).//範囲内か
+            Where(e => e.GetComponent<PlayerNumber>().PlayerNum != playerNum).//陣営が異なるか
+            OrderBy(e => Vector3.Distance(transform.position, e.transform.position)).ToArray();//並び替え
         }
         else
         {
             objs = null;
         }
     }
+
+    protected bool GetRandomPosition(Vector3 center, float range, out Vector3 result)
+    //ランダムに移動場所を取得
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Vector3 randomPoint = center + UnityEngine.Random.insideUnitSphere * range;
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+            {
+                result = hit.position;
+                return true;
+            }
+        }
+        result = Vector3.zero;
+        return false;
+    }
+
+    protected abstract void MoveRandom(float range);//移動処理
 }
--- Assets/Player/Script/MobTest.cs
+++ Assets/Player/Script/MobTest.cs
@@ -12,7 +12,6 @@ public class MobTest : AIBase
         RUNDOMWAIK,
         RUN
     }
-    [SerializeField]
     private MobPattern mobPattern;
 
     [SerializeField]
@@ -20,15 +19,24 @@ public class MobTest : AIBase
     [SerializeField]
     private int runDistance;
 
-    private Vector3 Mypos;
-    private Vector3 nextPos;
-    private float RandomRunPosrange = 25;
-    private float RandomWalkPosRange = 15;
-    private float playerDistance;//プレイヤーと次の移動場所の距離
-    private float nextPosDistance;//次の場所までの距離
+    protected float RandomRunPosrange = 25;
+    protected float RandomWalkPosRange = 15;
+
     private float dis;//プレイヤーが近ければ0それ以外５
     private int numberOfZombi;//変身するゾンビの番号
+    private bool getCaughtFlg = false;
 
+    public MobPattern _MobPattern
+    {
+        get { return mobPattern; }
+        set { mobPattern = value; }
+    }
+
+    public bool GetCaughtFlg
+    {
+        get { return getCaughtFlg; }
+        set { getCaughtFlg = value; }
+    }
 
     protected override void Start()
     {
@@ -39,44 +47,47 @@ public class MobTest : AIBase
     // Update is called once per frame
     protected override void Update()
     {
-        base.Update();
-        switch (mobPattern)
+        if (!getCaughtFlg)
         {
-            case MobPattern.WAIT:
-                Wait();
-                break;
-            case MobPattern.RUNDOMWAIK:
-                RundomWalk();
-                break;
-            case MobPattern.RUN:
-                Run();
-                break;
-        }
-
-        if (Vector3.Distance(nextPos, transform.position) < 4 || Mypos == nextPos || nextPos == Vector3.zero)
-        {
+            base.Update();
             switch (mobPattern)
             {
                 case MobPattern.WAIT:
+                    Wait();
                     break;
                 case MobPattern.RUNDOMWAIK:
-                    MoveRandom(RandomWalkPosRange);
+                    RundomWalk();
                     break;
                 case MobPattern.RUN:
-                    MoveRandom(RandomRunPosrange);
+                    Run();
                     break;
             }
-        }
 
-        if (targetMobs.Any())
-        {
-            if (Vector3.Distance(targetMobs[0].transform.position, transform.position) < 5)
+            if (Vector3.Distance(nextPos, transform.position) < 4 || Mypos == nextPos || nextPos == Vector3.zero)
             {
-                dis = 0;
+                switch (mobPattern)
+                {
+                    case MobPattern.WAIT:
+                        break;
+                    case MobPattern.RUNDOMWAIK:
+                        MoveRandom(RandomWalkPosRange);
+                        break;
+                    case MobPattern.RUN:
+                        MoveRandom(RandomRunPosrange);
+                        break;
+                }
             }
-            else { dis = 5; }
+
+            if (targetMobs.Any())
+            {
+                if (Vector3.Distance(targetMobs[0].transform.position, transform.position) < 5)
+                {
+                    dis = 0;
+                }
+                else { dis = 5; }
+            }
+            Mypos = transform.position;
         }
-        Mypos = transform.position;
     }
 
     protected override void OnTriggerEnter(Collider col)
@@ -147,42 +158,27 @@ public class MobTest : AIBase
             return;
         }
     }
-
-    bool GetRandomPosition(Vector3 center, float range, out Vector3 result)
+    protected override void MoveRandom(float range)
     {
-        for (int i = 0; i < 30; i++)
+        if (targetMobs.Any())
         {
-            Vector3 randomPoint = center + UnityEngine.Random.insideUnitSphere * range;
-            NavMeshHit hit;
-            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+            if (GetRandomPosition(transform.position, range, out nextPos))
             {
-                result = hit.position;
-                return true;
-            }
-        }
-        result = Vector3.zero;
-        return false;
-    }
+                targetDistance = Vector3.Distance(targetMobs[0].transform.position, nextPos);
+                nextPosDistance = Vector3.Distance(transform.position, nextPos);
 
-    void MoveRandom(float range)
-    {
-        if(GetRandomPosition(transform.position,range,out nextPos))
-        {
-            playerDistance = Vector3.Distance(targetMobs[0].transform.position, nextPos);
-            nextPosDistance = Vector3.Distance(transform.position, nextPos);
-
-            //プレイヤーに近い位置ならやり直す
-            if ((mobPattern==MobPattern.RUN&&playerDistance - dis < nextPosDistance )||
-            (mobPattern == MobPattern.RUNDOMWAIK && playerDistance < runDistance+2))
-            {
-                MoveRandom(range);
-            }
-            else
-            {
-                navMeshAgent.SetDestination(nextPos);
+                //プレイヤーに近い位置ならやり直す
+                if ((mobPattern == MobPattern.RUN && targetDistance - dis < nextPosDistance) ||
+                (mobPattern == MobPattern.RUNDOMWAIK && targetDistance < runDistance + 2))
+                {
+                    MoveRandom(range);
+                }
+                else
+                {
+                    navMeshAgent.SetDestination(nextPos);
+                }
             }
         }
     }
-
 }
 
